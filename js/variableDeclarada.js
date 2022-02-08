75bd5e9e4462cf6d9911369d632ce85c72e6ed39
Derly <sottoderly@gmail.com>
2022-02-08 14:12:39 -0500

    Ejercicio 3 condicionales

---
+++ js/variableDeclarada.js
@@ -0,0 +1,9 @@
+"use strict"
+var nombre = "Derly Soto"
+var genero = "femenino"
+
+if (genero == "femenino") {
+    console.log('Señora ' + nombre + 'buenas tardes BIENVENIDA!!')
+}else{
+    console.log('Señor ' + nombre + ' buenas tarde BIENVENIDO!!')
+}
